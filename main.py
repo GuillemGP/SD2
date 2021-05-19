d8fbe53a277fcef4f23c544afc01ce6711f73801
GuillemGP <76963941+GuillemGP@users.noreply.github.com>
2021-05-19 09:24:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from lithops import FunctionExecutor
 from lithops import Storage
 
+BUCKET = '2020sd'
+
+config = {'lithops' : {'storage_bucket' : BUCKET},
+
+          'ibm_cf':  {'endpoint': 'https://s3.eu-de.cloud-object-storage.appdomain.cloud',
+                      'api_key': 'hUKJ-4H0JyETfezS8XpZVEy8p6-BYhCsyPO8K8i_FVE9'},
+
+          'ibm_cos': {'region': 'eu-de',
+                      'api_key': 'hUKJ-4H0JyETfezS8XpZVEy8p6-BYhCsyPO8K8i_FVE9'}}
 
 def mostrarMenu():
     print("Quina opcio vols fer?")
@@ -13,29 +22,34 @@ def mostrarMenu():
     opcion = int(input())
     return opcion
 
+
 print('Practica 2 SD\n')
-print('Tractament de les dades pressupostaries de l\'ajuntament de Reus\n')
+print('Tractament de les dades pressupostaries de l\'ajuntament de Reus')
 print('Les dades pressupostaries de cada any es guarden en un fitxer csv\n')
 
-
+storage = Storage(config=config)
 
 opcion = mostrarMenu()
 while(opcion != 0):
     if opcion == 1: 
         print("Llista de fitxers guardats: ")
-        
+        print(storage.list_keys(BUCKET))
+    
     if opcion == 2: 
         print("Quin document vol afegir? ")
         fitxer = input()
-    
+
     if opcion == 3: 
         print("Quin document vol eliminar? ")
         fitxer = input()
+        storage.delete_object('2020sd', fitxer)
 
     if opcion == 4: 
         print("De quin any vols introduir noves dades? ")
         any = int(input())
-    
+
     if opcion == 5: 
         print("De quin any vols consultar les dades? ")
         any = int(input())
+    
+    opcion = mostrarMenu()
