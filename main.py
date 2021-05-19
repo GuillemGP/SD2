88e2fee1916c3ba120412d8df212ba3993c3c77e
GuillemGP <76963941+GuillemGP@users.noreply.github.com>
2021-05-19 20:00:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from lithops import FunctionExecutor
 from lithops import Storage
 
+import io
+
 BUCKET = '2020sd'
 
 config = {'lithops' : {'storage_bucket' : BUCKET},
 
           'ibm_cf':  {'endpoint': 'https://s3.eu-de.cloud-object-storage.appdomain.cloud',
+                      'namespace': 'guillem.gorgori@estudiants.urv.cat_dev',
                       'api_key': 'hUKJ-4H0JyETfezS8XpZVEy8p6-BYhCsyPO8K8i_FVE9'},
 
           'ibm_cos': {'region': 'eu-de',
@@ -15,12 +18,19 @@ def mostrarMenu():
     print("Quina opcio vols fer?")
     print("Opcion 0: Sortir")
     print("Opcion 1: Llistar documents bucket")
-    print("Opcion 2: Afegir document csv al bucket")
+    print("Opcion 2: Afegir un document csv al bucket")
     print("Opcion 3: Eliminar document csv al bucket")
-    print("Opcion 4: Afegir dada a un document csv")
-    print("Opcion 5: Realitzar consulta document")
+    print("Opcion 4: Afegir una nova dada a un document csv")
+    print("Opcion 5: Realitzar una consulta document")
     opcion = int(input())
     return opcion
+
+def my_function(obj_id, storage):
+    print(obj_id)
+
+    data = storage.get_cloudobject(obj_id)
+
+    return data.decode()
 
 
 print('Practica 2 SD\n')
@@ -36,11 +46,14 @@ while(opcion != 0):
         print(storage.list_keys(BUCKET))
     
     if opcion == 2: 
-        print("Quin document vol afegir? ")
+        print("Quin document vols afegir? ")
         fitxer = input()
 
+        f = open(fitxer, 'rb')
+        obj_id = storage.put_cloudobject(io.BytesIO(f.read()), BUCKET, fitxer)
+
     if opcion == 3: 
-        print("Quin document vol eliminar? ")
+        print("Quin document vols eliminar? ")
         fitxer = input()
         storage.delete_object('2020sd', fitxer)
 
