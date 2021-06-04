248128c9cbc27a428970cb50dd7b00ba3495b688
GuillemGP <76963941+GuillemGP@users.noreply.github.com>
2021-06-04 20:38:37 +0200

    Add files via upload

--- funcions.py
+++ funcions.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 def mostrarMenu():
     print("Quina opcio vols fer?")
     print("Opcio 0: Sortir")
@@ -6,8 +9,9 @@ def mostrarMenu():
     print("Opcio 3: Penjar un document al nuvol")
     print("Opcio 4: Eliminar document del nuvol")
     print("Opcio 5: Afegir una nova dada a un document csv")
-    print("Opcio 6: Generar grafic")
-    print("Opcio 7: Mostra per pantalla el contingut d'un fitxer del nuvol")
+    print("Opcio 6: Mostra per pantalla el contingut d'un fitxer del nuvol")
+    print("Opcion 7: Genera el grafic que volguis")
+    print("Opción 8: Genera el grafic d'exemple de 'PAGAMENTS REALITZATS' per 'Alcaldia-Òrgans de govern-Dietes organs govern'")
     opcion = int(input())
     return opcion
 
@@ -20,3 +24,20 @@ def inicialitzar():
     print('\n\nPractica 2 SD\n')
     print('Tractament de les dades pressupostaries de l\'ajuntament de Reus')
     print('Les dades pressupostaries de cada any es guarden en un fitxer csv\n')
+
+def getCsvData(values):
+    url = 'https://us-south.functions.cloud.ibm.com/api/v1/namespaces/victor.cano%40estudiants.urv.cat_dev/actions/plolt?blocking=true'
+    data = { 
+        "valorAConsultar": values["valorAConsultar"],
+        "columnaACercar": "DESCRIPCIÓ PARTIDA" , 
+        "idCerca": values["idCerca"]
+    }
+    jsonData = json.dumps(data)
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    r = requests.post(url, data=jsonData, headers=headers, auth=HTTPBasicAuth('8aaf2c73-23b9-4c80-abf1-855bffabd931', '8jiLkY0XH3lWxRnKfnwyIW2xT9v7UJUrJRmtblBfwTUfr3jKLfR5CsoZn6KjLsxz'))
+    print("S'ha rebut la resposta de la funcio del cloud")
+    response = json.loads(r.text)
+    result = {}
+    result["anys"] = response["response"]["result"]["anys"]
+    result["consultes"] = response["response"]["result"]["consultes"]
+    return result
--- main.py
+++ main.py
@@ -3,93 +3,38 @@ from lithops import Storage
 import matplotlib.pyplot as plt
 import pandas as pd
 import io
-import requests
-from requests.auth import HTTPBasicAuth
-import requests
-import json
 
-BUCKET = '2020sd'
-
-#IBM Cloud
-config = {'lithops' : {'storage_bucket' : BUCKET},
-
-          'ibm_cf':  {'endpoint': 'https://s3.eu-de.cloud-object-storage.appdomain.cloud',
-                      'namespace': 'guillem.gorgori@estudiants.urv.cat_dev',
-                      'api_key': 'hUKJ-4H0JyETfezS8XpZVEy8p6-BYhCsyPO8K8i_FVE9'},
-
-          'ibm_cos': {'region': 'eu-de',
-                      'api_key': 'hUKJ-4H0JyETfezS8XpZVEy8p6-BYhCsyPO8K8i_FVE9'}}
-
-def mostrarMenu():
-    print("Quina opcio vols fer?")
-    print("Opcion 0: Sortir")
-    print("Opcion 1: Llistar documents bucket")
-    print("Opcion 2: Crear un document al bucket en blanc")
-    print("Opcion 3: Penjar un document al bucket")
-    print("Opcion 4: Eliminar document csv al bucket")
-    print("Opcion 5: Afegir una nova dada a un document csv")
-    print("Opcion 6: Realitzar una consulta document")
-    print("Opcion 7: Invocar funcion IBM cloud")
-    print("Opcion 8: Genera el gráfico que quieras")
-    print("Opción 9: Genera el gráfico de ejemplo de 'PAGAMENTS REALITZATS' per 'Alcaldia-Òrgans de govern-Dietes organs govern'")
-    opcion = int(input())
-    return opcion
-
-def llistarArxiusBucket():
-    print("\nLlista de fitxers guardats al bucket: ")
-    print(storage.list_keys(BUCKET))
-    print("\n")
-
-def inicialitzar():
-    print('\n\nPractica 2 SD\n')
-    print('Tractament de les dades pressupostaries de l\'ajuntament de Reus')
-    print('Les dades pressupostaries de cada any es guarden en un fitxer csv\n')
-
-def getCsvData(values):
-    url = 'https://us-south.functions.cloud.ibm.com/api/v1/namespaces/victor.cano%40estudiants.urv.cat_dev/actions/plolt?blocking=true'
-    data = { 
-        "valorAConsultar": values["valorAConsultar"],
-        "columnaACercar": "DESCRIPCIÓ PARTIDA" , 
-        "idCerca": values["idCerca"]
-    }
-    jsonData = json.dumps(data)
-    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-    r = requests.post(url, data=jsonData, headers=headers, auth=HTTPBasicAuth('8aaf2c73-23b9-4c80-abf1-855bffabd931', '8jiLkY0XH3lWxRnKfnwyIW2xT9v7UJUrJRmtblBfwTUfr3jKLfR5CsoZn6KjLsxz'))
-    print("S'ha rebut la resposta de la funcio del cloud")
-    response = json.loads(r.text)
-    result = {}
-    result["anys"] = response["response"]["result"]["anys"]
-    result["consultes"] = response["response"]["result"]["consultes"]
-    return result
+import funcions
+import configIBMCloud
     
 if __name__ == '__main__':
-    inicialitzar()
-    storage = Storage(config=config)
-    opcion = mostrarMenu()
+    funcions.inicialitzar()
+    storage = Storage(config=configIBMCloud.config)
+    opcion = funcions.mostrarMenu()
     while(opcion != 0):
         if opcion == 1: 
-            llistarArxiusBucket()
+            funcions.llistarArxiusBucket()
         
         if opcion == 2: 
             print("\nQuin nom vols que tingui el document nou al bucket? ")
             fitxer = input()
-            obj_id = storage.put_cloudobject('', BUCKET, fitxer)
+            obj_id = storage.put_cloudobject('', configIBMCloud.BUCKET, fitxer)
         
         if opcion == 3: 
             print("\nQuin document vols penjar? ")
             fitxer = input()
 
             f = open(fitxer, 'rb')
-            obj_id = storage.put_cloudobject(io.BytesIO(f.read()), BUCKET, fitxer)
+            obj_id = storage.put_cloudobject(io.BytesIO(f.read()), configIBMCloud.BUCKET, fitxer)
 
         if opcion == 4: 
-            llistarArxiusBucket()
+            funcions.llistarArxiusBucket()
             print("\nQuin document vols eliminar? ")
             fitxer = input()
-            storage.delete_object(BUCKET, fitxer)
+            storage.delete_object(configIBMCloud.BUCKET, fitxer)
 
         if opcion == 5: 
-            llistarArxiusBucket()
+            funcions.llistarArxiusBucket()
             print("\nA quin fitxer vols introduir noves dades? ")
             fitxer = input()
 
@@ -116,23 +61,22 @@ if __name__ == '__main__':
             print("Introudeix les obligacions:")
             info = info + ", " + input()
 
-            contingut=storage.get_object(bucket=BUCKET, key=fitxer)
+            contingut=storage.get_object(bucket=configIBMCloud.BUCKET, key=fitxer)
             contingut=contingut.decode()
 
             contingut = contingut + "\n" + info
 
-            obj_id = storage.put_cloudobject(contingut, BUCKET, fitxer)
-
-        if opcion == 6: 
-            print("\nDe quin any vols consultar les dades? ")
-            any = int(input())
-
-        if opcion == 7: 
-            response = json.loads(requests.get("https://eu-gb.functions.appdomain.cloud/api/v1/web/guillem.gorgori%40estudiants.urv.cat_dev/SD/hello.json").text)
-            print("\n")
-            print(response)
-            print("\n")
-        if opcion == 8:
+            obj_id = storage.put_cloudobject(contingut, configIBMCloud.BUCKET, fitxer)
+        
+        if opcion == 6:
+            funcions.llistarArxiusBucket()
+            print("\nQuin fitxer vols mostrar per pantalla? ")
+            fitxer = input()
+            contingut=storage.get_object(bucket=configIBMCloud.BUCKET, key=fitxer)
+            contingut=contingut.decode()
+            print("\nContingut del fitxer:\n" + contingut + "\n")
+        
+        if opcion == 7:
             print("Introdueix el nom de la columna a analitzar (per exemple 'PAGAMENTS REALITZATS')")
             col = input()
             print("Introdueix un valor de alguna fila de 'DESCRIPCIO PARTIDA' per a analitzar")
@@ -142,26 +86,31 @@ if __name__ == '__main__':
                 "columnaACercar": "DESCRIPCIÓ PARTIDA" , #columna de la que volem buscar el id
                 "idCerca": row #valor del csv del que volem extreure dades
             }
-            r = getCsvData(values)
+            r = funcions.getCsvData(values)
             plt.figure()
             plt.plot(r["anys"], r["consultes"])
             plt.xlabel(values["valorAConsultar"], fontsize=18)
             plt.ylabel('Anys', fontsize=16)
+            #plt.savefig("./images/" + col + "Reus.png")                       #Si es un windows usar este formato
+            plt.savefig("/Users/guillemgorgoriperez/Desktop/DespesesReus.png") #Si es un macos usar este formato
             plt.savefig("./images/" + col + "Reus.png")
             print("Gràfic generat a ./images")
             print()
-        if opcion == 9:
+
+        if opcion == 8:
             values = { 
                 "valorAConsultar": "PAGAMENTS REALITZATS", #valor que volem analitzar graficament
                 "columnaACercar": "DESCRIPCIÓ PARTIDA" , #columna de la que volem buscar el id
                 "idCerca": "Alcaldia-Òrgans de govern-Dietes organs govern" #valor del csv del que volem extreure dades
             }
-            r = getCsvData(values)
+            r = funcions.getCsvData(values)
             plt.figure()
             plt.plot(r["anys"], r["consultes"])
             plt.xlabel(values["valorAConsultar"], fontsize=18)
             plt.ylabel('Anys', fontsize=16)
-            plt.savefig("./images/DespesesReus.png")
+            #plt.savefig("./images/DespesesReus.png")                          #Si es un windows usar este formato
+            lt.savefig("/Users/guillemgorgoriperez/Desktop/DespesesReus.png") #Si es un macos usar este formato
             print("Gràfic generat a ./images")
             print()
-        opcion = mostrarMenu()
+        
+        opcion = funcions.mostrarMenu()
