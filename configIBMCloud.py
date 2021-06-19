7d503591c7897ffeb4a18127fda58d48837ee277
GuillemGP <76963941+GuillemGP@users.noreply.github.com>
2021-06-19 09:54:45 +0200

    Add files via upload

--- configIBMCloud.py
+++ configIBMCloud.py
@@ -1,12 +1,12 @@
 #Name bucket
-BUCKET = '2020sd'
+BUCKET = 'my-bucket'
 
 #IBM Cloud
 config = {'lithops' : {'storage_bucket' : BUCKET},
 
-          'ibm_cf':  {'endpoint': 'https://s3.eu-de.cloud-object-storage.appdomain.cloud',
-                      'namespace': 'guillem.gorgori@estudiants.urv.cat_dev',
-                      'api_key': 'hUKJ-4H0JyETfezS8XpZVEy8p6-BYhCsyPO8K8i_FVE9'},
+          'ibm_cf':  {'endpoint': 'my-endpoint',
+                      'namespace': 'my-namespace',
+                      'api_key': 'my-api_key'},
 
-          'ibm_cos': {'region': 'eu-de',
-                      'api_key': 'hUKJ-4H0JyETfezS8XpZVEy8p6-BYhCsyPO8K8i_FVE9'}}
+          'ibm_cos': {'region': 'my-region',
+                      'api_key': 'my-api_key'}}
